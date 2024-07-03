534ecc1e9aacc35202404e68341cd9050325fc01
gen-hashimoto <egghershey+portfolio@gmail.com>
2024-07-04 08:19:20 +0900

    add: tree-sitter / liaLIne

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,40 @@
+-- https://github.com/tadashi-aikawa/owl-playbook/blob/e597c35e875579b3caecaa76f11d5c6d244198b1/mnt/common/nvim/lua/plugins/nvim-treesitter.lua
+return {
+  'nvim-treesitter/nvim-treesitter',
+  event = { 'BufNewFile', 'BufRead' },
+  build = ":TSUpdate",
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = {
+        "bash",
+        "css",
+        "diff",
+        "dockerfile",
+        "elixir",
+        "gitignore",
+        "go",
+        "html",
+        "http",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "rust",
+        "svelte",
+        "toml",
+        "typescript",
+        "vim",
+        "vue",
+        "yaml",
+      },
+      highlight = {
+        enable = true,
+        disable = { "ini" }
+      },
+      autotag = {
+        enable = true,
+      },
+    }
+  end
+}
