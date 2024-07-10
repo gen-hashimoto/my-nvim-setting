f1e2408d42060ac6a316c774bdbcaef6b975e49a
gen-hashimoto <egghershey+portfolio@gmail.com>
2024-07-11 08:28:16 +0900

    add: plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,37 +7,43 @@ return {
     local lualine = require('lualine')
     local config = {
       options = {
-        component_separators = {},
-        section_separators = {},
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        }
       },
       sections = {
-        lualine_a = {
-          'branch',
-          {
-            'diff',
-            symbols = { added = ' ', modified = ' ', removed = ' ' },
-          },
-        },
-        lualine_b = { { 'filename', path = 1 }, 'aerial' },
-        lualine_c = {
-          "'%='",
-          {
-            'diagnostics',
-            symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
-          },
-        },
-        lualine_x = { 'encoding', 'fileformat' },
-        lualine_y = { 'filetype', 'searchcount' },
-        lualine_z = {},
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
         lualine_y = {},
-        lualine_z = {},
+        lualine_z = {}
       },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {}
     }
     lualine.setup(config)
   end
