1118456467ec51d033580953602f999a0f7f8d63
gen-hashimoto <egghershey+portfolio@gmail.com>
2024-07-11 07:36:59 +0900

    fix: separate a file

--- lua/plugins/textobj.lua
+++ lua/plugins/textobj.lua
@@ -1,7 +1,4 @@
 return {
-    -- vim-surround
-    "tpope/vim-surround",
-
     -- textobj
     "kana/vim-textobj-user",
     { "kana/vim-textobj-entire", dependencies = { "kana/vim-textobj-user" }},
