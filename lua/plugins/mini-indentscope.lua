59bbb6c89a0a38e38e6b0153ac54aa64abe2450a
kpis-hashimoto-gen <hashimoto.gen.kpis@gmail.com>
2024-07-12 12:51:15 +0900

    fix: mini-indentscope setting

--- lua/plugins/mini-indentscope.lua
+++ lua/plugins/mini-indentscope.lua
@@ -3,12 +3,16 @@ return   {
     version = '*',
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      require('mini.indentscope').setup(
-        {
-          -- Which character to use for drawing scope indicator
-          -- '│', '|', '¦', '┆', '┊', ''
-          symbol = '│',
-        }
-      )
+        require('mini.indentscope').setup(
+            {
+                -- Which character to use for drawing scope indicator
+                -- '│', '|', '¦', '┆', '┊', ''
+                symbol = '│',
+                draw = {
+                  delay = 0,
+                  animation = require("mini.indentscope").gen_animation.none(),
+                },
+            }
+        )
     end
-  }
+}
