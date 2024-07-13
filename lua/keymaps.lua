350282d32717556ba880142712c20a3eb41a5010
gen-hashimoto <egghershey+portfolio@gmail.com>
2024-07-13 19:10:29 +0900

    add: keymap for buffer

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,3 +7,7 @@ vim.g.maplocalleader = " "
 
 -- open nvim-tree with <leader>e
 map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+
+-- moving buffer
+map("n","<leader>n", ":bnext<Return>", opts)
+map("n", "<leader>p", ":bprevious<Return>", opts)
