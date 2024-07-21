7cd9e193ab5a41e4fa81f09c3ab309e682544f69
gen-hashimoto <egghershey+portfolio@gmail.com>
2024-07-22 07:58:20 +0900

    fix: display gitignored files

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,10 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
-        require("nvim-tree").setup {}
+        require("nvim-tree").setup({
+            filters = {
+                git_ignored = false,
+            }
+        })
     end,
 }
