4a099aa92d8eb12b8655a8db46e347bd5d8746eb
gen-hashimoto <egghershey+portfolio@gmail.com>
2024-07-25 07:09:57 +0900

    add: nvim-notify

---
+++ lua/plugins/nvim-notify.lua
@@ -0,0 +1,15 @@
+return {
+  'rcarriga/nvim-notify',
+  config = function()
+    local notify = require 'notify'
+    notify.setup()
+    vim.notify = notify
+
+    local telescope = require 'telescope'
+    telescope.load_extension 'notify'
+    vim.keymap.set('n', '<leader>fn', function()
+      telescope.extensions.notify.notify()
+    end)
+
+  end,
+}
